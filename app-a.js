88dfc42b62c6204c51fbee3e2dc4c27031fb3cd1
unmeimusu <unmeimusu@gmail.com>
2023-04-20 20:51:46 +0700

    Update file

--- app-a.js
+++ app-a.js
@@ -1,13 +1,9 @@
-import { reactive, html } from './a/core@1.0.0-alpha.9';
+// import { reactive, html } from './a/core@1.0.0-alpha.9';
+import { reactive, html } from 'https://esm.sh/@arrow-js/core';
 
 const data = reactive({
-    price: 25,
-    quantity: [10,12,15]
-  })
+  price: 25,
+  quantity: 10
+})
 
-html`
-  <ul>
-    <li>Hello ${data.price} (🪨 static expression)</li>
-    <li>Hello ${() => data.quantity} (⚡ dynamic expression)</li>
-  </ul>
-`
+console.log(data.price);

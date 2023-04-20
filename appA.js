cc6b6b54ca320740c2233b9ce8513d4ce8b00437
unmeimusu <unmeimusu@gmail.com>
2023-04-21 06:37:01 +0700

    Update file

--- appA.js
+++ appA.js
@@ -7,7 +7,9 @@ const data = reactive({
 
 // Reactive Data
 // Will be changed for each updated value
-// observe mutations to their properties
+// $on : observe mutations to their properties
+
+// Example 1: Observe single property
 data.$on('price',(value)=>{
   console.log(`Price changed to ${data.price}`)
 })
@@ -15,4 +17,25 @@ data.$on('price',(value)=>{
 data.price = 35
 
 console.log(data.price);
-// 
+
+// Example 2: Observe multiple properties
+const data1 = reactive({
+  price: 25,
+  quantity: 10,
+  logTotal: true
+})
+
+function total () {
+  if (data1.logTotal) {
+    console.log(`Total: ${data1.price * data1.quantity}`)
+  }
+}
+// observe properties and then execute function
+data1.$on('price',total)
+data1.$on('quantity', total)
+data1.$on('logTotal', total)
+total()
+data1.price = 35
+
+data1.$off('quantity', total)
+data1.quantity = 20

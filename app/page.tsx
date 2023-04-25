92c384f00512cb94c211a1164f9baeae857150bb
unmeimusu <unmeimusu@gmail.com>
2023-04-25 00:20:21 +0000

    Apr 25, 2023, 7:20 AM

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    return <div>
+        <ul>
+            <a href="./index.html"><li>Index</li></a>
+            <a href="./page-a.html"><li>A page</li></a>
+            <a href="./page-r.html"><li>R page</li></a>
+        </ul>
+        <h1>Hello, Next.js!</h1>
+    </div>
 }

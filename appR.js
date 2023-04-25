c5561606cb55202a2b3808b9aa554b967d3c494c
unmeimusu <unmeimusu@gmail.com>
2023-04-25 01:06:00 +0000

    Apr 25, 2023, 8:06 AM

--- appR.js
+++ appR.js
@@ -1,4 +1,16 @@
 ReactDOM.render(
     <h1>Hello, React!</h1>,
-    document.getElementById("container")
+    document.getElementById("root")
 )
+
+let counter = 0
+
+function show() {
+    counter++
+    const el = <p>{counter}</p>
+    ReactDOM.render(
+        el,document.getElementById("root")
+    )
+}
+
+setInterval(show,1000)

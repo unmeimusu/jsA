72b00a40042d61d89b97f5abb19092d509990a32
unmeimusu <unmeimusu@gmail.com>
2023-04-25 11:18:34 +0000

    Apr 25, 2023, 6:18 PM

--- appR.js
+++ appR.js
@@ -205,7 +205,7 @@ ReactDOM.render(
     document.getElementById("r10")
 )
 
-// List : print listed items to the DOM
+// List with index : print listed items to the DOM
 const al = ["A","B","C"]
 
 function Myl(props) {

af65a30077e9250c88b0fab4e24abea961209b25
unmeimusu <unmeimusu@gmail.com>
2023-04-25 14:24:47 +0000

    Apr 25, 2023, 9:24 PM

--- appR.js
+++ appR.js
@@ -230,8 +230,10 @@ function Addform(props) {
         setItem(e.target.value) // set item from the form value
     }
     function handleSubmit(e) {
-        // to prevent the default event on cancelable events
+        // props after itemManager component build
         props.handleSubmit(item)
+        setItem("")
+        // to prevent the default event on cancelable events
         e.preventDefault()
     }
     return (

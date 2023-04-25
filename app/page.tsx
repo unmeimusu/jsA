ad611b373d157c9e5dcadd959a4eaf0da3a3645a
unmeimusu <unmeimusu@gmail.com>
2023-04-25 07:46:21 +0000

    Apr 25, 2023, 2:46 PM

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+"use client"
+
 import {useState} from "react"
 
 export default function Page() {
     const [message,setMessage] = useState("hello")
 
     return <div>
-        <h1>Hello, Next.js!</h1>
+        <h1>Hello, Next.js! {message}</h1>
         <div id="root"></div>
     </div>
 }

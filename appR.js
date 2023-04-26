253a6223ce12efd08efada87b43d1b606448f02d
unmeimusu <unmeimusu@gmail.com>
2023-04-26 04:14:39 +0000

    Apr 26, 2023, 11:14 AM

--- appR.js
+++ appR.js
@@ -304,7 +304,7 @@ function Form() {
 
     const handlSubmt = (e) => {
         if (item !== "") {
-            props.handlSubmt(item)
+            props.handlSubmt(item) 
             setItem("")
         }
         e.preventDefault()
